c40b24727c456eff0a938700128a95f3e2a85691
Zach Frew <zachary.frew@ascension-external.org>
2020-02-04 15:33:01 -0600

    Setup UI

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -1,20 +1,26 @@
-//
-//  ViewController.swift
-//  Project1
-//
-//  Created by Zachary Frew on 12/11/19.
-//  Copyright © 2019 Zach Frew. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var model: UISegmentedControl!
+    @IBOutlet weak var upgrades: UISegmentedControl!
+    @IBOutlet weak var mileageLabel: UILabel!
+    @IBOutlet weak var mileage: UISlider!
+    @IBOutlet weak var condition: UISegmentedControl!
+    @IBOutlet weak var valuation: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        stackView.setCustomSpacing(30, after: model)
+        stackView.setCustomSpacing(30, after: upgrades)
+        stackView.setCustomSpacing(30, after: mileage)
+        stackView.setCustomSpacing(60, after: condition)
     }
-
-
+    
+    @IBAction func calculateValue(sender: UISegmentedControl) {
+        print("calculating")
+    }
+    
 }
 
