9e9a7e00d4f31dcb9533b297a198ebd66c77cc12
Zach Frew <zachary.frew@ascension-external.org>
2020-02-06 13:46:03 -0600

    Finish project 1

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -9,6 +9,8 @@ final class ViewController: UIViewController {
     @IBOutlet weak var condition: UISegmentedControl!
     @IBOutlet weak var valuation: UILabel!
     
+    let cars = Cars()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,11 +18,24 @@ final class ViewController: UIViewController {
         stackView.setCustomSpacing(30, after: upgrades)
         stackView.setCustomSpacing(30, after: mileage)
         stackView.setCustomSpacing(60, after: condition)
+        
+        calculateValue(sender: self)
     }
     
-    @IBAction func calculateValue(sender: UISegmentedControl) {
-        print("calculating")
+    @IBAction func calculateValue(sender: Any) {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        let formattedMileage = formatter.string(for: mileage.value) ?? "0"
+        mileageLabel.text = "MILEAGE (\(formattedMileage) miles)"
+        
+        if let prediction = try? cars.prediction(model: Double(model.selectedSegmentIndex), premium: Double(upgrades.selectedSegmentIndex), mileage: Double(mileage.value), condition: Double(condition.selectedSegmentIndex)) {
+            let clampedValuation = max(2000, prediction.price)
+            
+            formatter.numberStyle = .currency
+            valuation.text = formatter.string(for: clampedValuation)
+        } else {
+            valuation.text = "Error"
+        }
     }
-    
 }
-
