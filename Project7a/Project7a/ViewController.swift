853ea8b75fbc12146aab54d68f1b0dcc9adeebd5
Zach Frew <zachary.frew@ascension-external.org>
2020-03-10 15:02:48 -0500

    Project7a setup - CoreML

---
+++ Project7a/Project7a/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7a
+//
+//  Created by Zachary Frew on 3/10/20.
+//  Copyright © 2020 Zach Frew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
